63d70b296d9f353b16e61708213174559037fa99
youth2ee <sprirealee@gmail.com>
2019-10-31 16:00:34 +0900

    191031

---
+++ src/com/naver/join/JoinDTO.java
@@ -0,0 +1,52 @@
+package com.naver.join;
+
+public class JoinDTO {
+	
+	private String id;
+	private String pw;
+	private String name;
+	private String email;
+	private String phone;
+	private int grade;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+	
+	
+
+}
